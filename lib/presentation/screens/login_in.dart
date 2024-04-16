9ff118ddc3be46f09a7eeab4becd624bd52d2a04
SosiSis <sosinasisay30@gmail.com>
2024-04-16 23:15:29 +0300

    my commit

---
+++ lib/presentation/screens/login_in.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class LogInPage extends StatelessWidget {
+  const LogInPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: Image.asset('assets/component.png',
+                  alignment: Alignment.center, fit: BoxFit.fill
+
+                  // Set width to occupy full width
+                  ),
+            ),
+            Container(
+                margin: EdgeInsets.all(20.0),
+                // width: 500,
+                child: Column(
+                  children: [
+                    Text("Log In",
+                        style: TextStyle(
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black)),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        prefixIcon: Icon(Icons.mail),
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        prefixIcon: Icon(Icons.lock),
+                      ),
+                      obscureText: true,
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    Text(
+                      'I do not have account',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.blue),
+                      ),
+                      onPressed: () {
+                        print('Button clicked');
+                      },
+                      child: Text(
+                        'Log In',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                )),
+            Container(child: Image.asset('assets/component1.png'))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/screens/sign_up.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: Image.asset('assets/component.png',
+                  alignment: Alignment.center, fit: BoxFit.fill
+
+                  // Set width to occupy full width
+                  ),
+            ),
+            Container(
+                margin: EdgeInsets.all(20.0),
+                // width: 500,
+                child: Column(
+                  children: [
+                    Text("Sign Up",
+                        style: TextStyle(
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black)),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                        prefixIcon: Icon(Icons.person),
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        prefixIcon: Icon(Icons.mail),
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        prefixIcon: Icon(Icons.lock),
+                      ),
+                      obscureText: true,
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    Text(
+                      'I already have an account',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.blue),
+                      ),
+                      onPressed: () {
+                        print('Button clicked');
+                      },
+                      child:
+                          Text('Submit', style: TextStyle(color: Colors.white)),
+                    ),
+                  ],
+                )),
+            Container(child: Image.asset('assets/component1.png'))
+          ],
+        ),
+      ),
+    );
+  }
+}
